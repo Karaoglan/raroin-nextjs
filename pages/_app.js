9b2234a445ee0615ecb57bf5af6b62988fd9b3e9
Burak Karaoglan <burakkaraoglan55@gmail.com>
2022-07-12 09:51:47 +0300

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import "../assets/css/plugins/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
+import "../assets/scss/style.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
