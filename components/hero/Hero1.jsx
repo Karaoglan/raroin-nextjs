b83e8200d174ffd0377a11b72f725fbffde0fb95
Burak Karaoglan <burakkaraoglan55@gmail.com>
2022-08-17 12:56:23 +0300

    add design 2

--- components/hero/Hero1.jsx
+++ components/hero/Hero1.jsx
@@ -42,10 +42,10 @@ const Hero1 = () => {
                   using NFT&apos;s & blockchain technology
                 </p>
                 <div
-                  className="d-flex flex-column flex-md-row
+                  className="d-flex flex-column flex-md-row md:space-x-20
                 sm:space-y-20">
                     <Link href="marketplace">
-                      <button className="btn btn-primary mr-20"><span className='p-40'>Explore</span></button>
+                      <button className="btn btn-primary"><span className='p-40'>Explore</span></button>
                     </Link>
 
                     <Link href="upload">
